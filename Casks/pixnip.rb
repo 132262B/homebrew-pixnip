268b20a53e061b50a5ce52656be321647b59f259
Igor <132262b@naver.com>
2026-09-24 18:14:16 +0900

    pixnip 1.0.5

--- Casks/pixnip.rb
+++ Casks/pixnip.rb
@@ -7,8 +7,8 @@
 # 132262B/homebrew-pixnip 는 릴리스 zip 이 올라가는 공개 탭 저장소다. 소스 저장소는 비공개라
 # 여기서 참조하지 않는다.
 cask "pixnip" do
-  version "1.0.4"
-  sha256 "e5a979785e7013ae913c175762f5f99935d0b270d399061b508164f3e3cb35ba"
+  version "1.0.5"
+  sha256 "6deb1ce4b47afb047d1a56cf1592aeb04d7b53e5f33377c84c6e2077e8ad2d25"
 
   url "https://github.com/132262B/homebrew-pixnip/releases/download/v#{version}/pixnip-#{version}-macos.zip",
       verified: "github.com/132262B/homebrew-pixnip/"
